8cc51b92ae3123bf447dcb2e1d4c795eb992fc17
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-11 15:19:28 -0300

    feat: add model in area admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from contacts.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+        'created_date',
+    )
+    list_filter = ('created_date', 'last_name')
+    search_fields = ('first_name', 'last_name', 'phone', 'email')
+    ordering = ('last_name', 'first_name', 'created_date')
+    date_hierarchy = 'created_date'
+    fields = ('first_name', 'last_name', 'phone', 'email', 'description')
+    readonly_fields = ('created_date',)
