7f687c9471d7fc6c650709c0da760b0387e10012
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-11 15:06:05 -0300

    feat: create model contact

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    firt_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True, null=True)
