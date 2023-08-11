b7b2b569d15e6918082e88037ab242d288b62d35
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-11 15:35:00 -0300

    feat: add field editable area admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -6,15 +6,19 @@ from contacts.models import Contact
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'first_name',
         'last_name',
         'phone',
         'email',
         'created_date',
     )
-    list_filter = ('created_date', 'last_name')
-    search_fields = ('first_name', 'last_name', 'phone', 'email')
-    ordering = ('last_name', 'first_name', 'created_date')
+    list_filter = ('created_date',)
+    search_fields = ('id', 'first_name', 'last_name', 'phone', 'email')
+    ordering = ('-id', 'last_name', 'first_name', 'created_date')
     date_hierarchy = 'created_date'
     fields = ('first_name', 'last_name', 'phone', 'email', 'description')
     readonly_fields = ('created_date',)
+    list_per_page = 10
+    list_max_show_all = 100
+    list_display_links = ('id', 'first_name')
