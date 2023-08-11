f7fde68aff3a51b160fedb20239ed8f4e66658c1
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-11 15:19:06 -0300

    fix: rename field first name

--- contacts/models.py
+++ contacts/models.py
@@ -3,9 +3,12 @@ from django.utils import timezone
 
 
 class Contact(models.Model):
-    firt_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=20)
     email = models.EmailField(blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
