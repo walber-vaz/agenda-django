34ad1af1c8a528861db4840f979ba085cc8a905e
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-12 17:34:44 -0300

    feat: create model and migration

--- contacts/models.py
+++ contacts/models.py
@@ -2,16 +2,26 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=20)
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField(max_length=150)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True, null=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(
         upload_to='pictures/%Y/%m/', blank=True, null=True
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True
     )
 
     def __str__(self) -> str:
