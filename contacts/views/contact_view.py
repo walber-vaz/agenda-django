37612f8da58441d588ea8c92699a66cc3602697e
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-16 08:35:57 -0300

    feat: create search field

--- contacts/views/contact_view.py
+++ contacts/views/contact_view.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contacts.models import Contact
 
@@ -17,3 +18,25 @@ def contact(request, contact_id):
     context = {"contact": single_contact}
 
     return render(request, "contacts/contact.html", context)
+
+
+def search(request):
+    query = request.GET.get("q", "").strip()
+
+    if query == "" or len(query) <= 0:
+        return redirect("contacts:index")
+
+    contacts = (
+        Contact.objects.filter(
+            Q(first_name__icontains=query)
+            | Q(last_name__icontains=query)
+            | Q(email__icontains=query)
+            | Q(phone__icontains=query)
+        )
+        .filter(show=True)
+        .order_by("-id")
+    )
+
+    context = {"contacts": contacts}
+
+    return render(request, "contacts/index.html", context)
